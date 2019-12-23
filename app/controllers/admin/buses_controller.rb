b901a421999d9cfed94474f01990ec2a9fbd8c3f
Shubham <shubham@gmail.com>
2019-12-23 18:39:21 +0530

    try rebase

--- app/controllers/admin/buses_controller.rb
+++ app/controllers/admin/buses_controller.rb
@@ -14,7 +14,7 @@ class Admin::BusesController < ApplicationController
   end
 
   private
-
+  
     def require_admin
       unless current_user.admin?
         redirect_to root_path, notice:'Access Denied'
