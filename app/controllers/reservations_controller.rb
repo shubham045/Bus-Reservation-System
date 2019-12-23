4cb7e8b9f9a2a6f1b0eef7fd9ef9253c4529f9fb
Shubham <shubham@gmail.com>
2019-12-23 16:33:14 +0530

    9

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -48,27 +48,27 @@ class ReservationsController < ApplicationController
         end
   end
 
-    def new_seat
-        @bus = Bus.find(params[:bus_id])
-        @reservation = @bus.reservations.new
-        @date = params[:date]
-        @total_seats = Array(1..@bus.total_seats)
-        @count_seat = 0
-        for i in 1..@total_seats.size do
-            @count_seat += available_seat?(i,@date) ? 1 : 0
-        end
+  def new_seat
+    @bus = Bus.find(params[:bus_id])
+    @reservation = @bus.reservations.new
+    @date = params[:date]
+    @total_seats = Array(1..@bus.total_seats)
+    @count_seat = 0
+    for i in 1..@total_seats.size do
+        @count_seat += available_seat?(i,@date) ? 1 : 0
     end
+  end
 
-    def available_seat?(seat_num,date)
-        reservation = @bus.reservations.where(date:date)
-        reservation.each do |check_reservation|
-          seats = check_reservation.seats
-            if seats.map(&:seat_no).include?(seat_num)
-              return false
-            end
+  def available_seat?(seat_num,date)
+    reservation = @bus.reservations.where(date:date)
+    reservation.each do |check_reservation|
+      seats = check_reservation.seats
+        if seats.map(&:seat_no).include?(seat_num)
+          return false
         end
-        return true
     end
+    return true
+  end
 
 
 
