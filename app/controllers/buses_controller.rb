3ac5abe58ab34d27fb97051b135be351d080bee0
Shubham <shubham@gmail.com>
2019-12-23 17:34:06 +0530

    3 in new_work

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,7 +1,7 @@
 class BusesController < ApplicationController
 
-  def index
-    @buses=Bus.avilable_buses_for_all_reservations(params)
-  end
+	  def index
+	    @buses=Bus.avilable_buses_for_all_reservations(params)
+	  end
 
 end
