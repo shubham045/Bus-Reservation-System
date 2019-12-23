d860f0747a184e1dbb5c47a63faefc2816d05d59
Shubham <shubham@gmail.com>
2019-12-23 18:30:04 +0530

    m1

--- app/controllers/admin/buses_controller.rb
+++ app/controllers/admin/buses_controller.rb
@@ -14,9 +14,11 @@ class Admin::BusesController < ApplicationController
   end
 
   private
+  
     def require_admin
       unless current_user.admin?
         redirect_to root_path, notice:'Access Denied'
       end
     end
+
 end
