d3410ebbf5fb3b97470ad700a11984d5eba3f60c
Shubham <shubham@gmail.com>
2019-12-24 10:43:58 +0530

    testing

--- app/controllers/admin/buses_controller.rb
+++ app/controllers/admin/buses_controller.rb
@@ -14,7 +14,7 @@ class Admin::BusesController < ApplicationController
 
   private
     def require_admin
-      unless current_user.admin?
+      unless current_user.admin? && ture
         redirect_to root_path, notice:'Access Denied'
       end
     end
